e4967cf51b53fc12b6ddfee3f3a2d2740a59c4b8
Edwin Donovan <edwinj3chidoman@gmail.com>
2026-02-13 15:19:54 -0600

    Activamos Arcade Physics en la configuración del juego. Creamos un sprite con cuerpo físico. Detectamos las flechas del teclado. Movimos el jugador usando setVelocity. Evitamos que el personaje salga de la pantalla. Probamos distintas velocidades (normal y exagerada).

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const config = {
   width: 800,
   height: 450,
   backgroundColor: "#eb0000",
+  physics:{
+    default: "arcade",
+    arcade: {debug : false}
+  },
   scene: {
     preload,
     create,
@@ -11,27 +15,40 @@ const config = {
 };
 const game = new Phaser.Game(config);
 
-let movingText;
+let player;
+let cursors;
+let SPEED = 200;
+
 
 function preload() {
-  this.load.image("logo", "./atardecer.jpeg");
+    this.load.image("Player", "./assets/Player.png");
 }
 
 function create() {
-  movingText = this.add.text(40, 40, "Hola Coders", {
-    fontSize: "32px",
-    color: "#ffffff",
-  });
-
-  const img = this.add.image(400, 250, "logo");
-
-  img.setScale(0.3);
+    player = this.physics.add.sprite(400, 250, "Player");
+    player.setScale(0.15);
+    player.body.setSize(player.width * 0.6, player.height * 0.8, true);
+    player.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  movingText.x += 0.5;
+    player.setVelocity(0);
 
-  if (movingText.x > config.width) {
-    movingText.x = -movingText.width;
-  }
+    if(cursors.left.isDown){
+        player.setVelocityX(-SPEED); 
+    }
+    else if(cursors.right.isDown){
+        player.setVelocityX(SPEED);
+    }
+
+    if(cursors.up.isDown){
+        player.setVelocityY(-SPEED);
+    }
+    else if(cursors.down.isDown){
+        player.setVelocityY(SPEED);
+    }
+    if (player.body.velocity.length()>0){
+        player.body.velocity.normalize().scale(SPEED);
+    }
 }
